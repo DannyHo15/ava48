e7765976138a1758aa19638a6e593b83ccc7b546
Minh Luan <minhluan@MacBook-Pro.local>
2025-07-28 16:45:59 +0700

    update env

---
+++ deployment.staging.sh
@@ -0,0 +1,16 @@
+export ECR_REPOSITORY="ai-avatar/staging/ai-avatar-landingpage"
+export IMAGE_TAG="latest"
+
+# Log in to ECR
+aws ecr get-login-password --region ap-northeast-1 | sudo docker login --username AWS --password-stdin 023889582722.dkr.ecr.ap-northeast-1.amazonaws.com
+
+# Pull latest image from ECR
+sudo docker pull 023889582722.dkr.ecr.ap-northeast-1.amazonaws.com/$ECR_REPOSITORY:$IMAGE_TAG
+
+# Stop and remove old container
+sudo docker stop ai_avatar_landingpage || true
+sudo docker rm ai_avatar_landingpage || true
+
+# Run new container
+cd /usr/local/docker-composes/ai_avatar_landingpage
+sudo docker compose up -d
