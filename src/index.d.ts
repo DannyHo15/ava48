a85c2b8752d0d36dadb82a8acc19f5aa48a6b4ed
Văn Lợi Nguyễn <loi.nguyen.van@airstage-vn.asia>
2025-11-06 15:20:02 +0700

    fix env

--- src/index.d.ts
+++ src/index.d.ts
@@ -13,6 +13,13 @@ declare module "*.svg" {
 declare let process: {
   env: {
     NEXT_PUBLIC_APP_URL: string;
+    NEXT_PUBLIC_FIREBASE_API_KEY: string;
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    NEXT_PUBLIC_FIREBASE_APP_ID: string;
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: string;
     NODE_ENV: string;
     VERSION: string;
   };
