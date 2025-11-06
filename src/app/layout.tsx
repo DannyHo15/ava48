fd84c02245f9e86d85d30d6decbea4a7d725bb19
Van Quang <quang.tran.van@airstage-vn.asia>
2025-11-06 17:28:23 +0700

    update gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,7 +98,7 @@ export default async function RootLayout({
         {children}
         <FirebaseAnalytics />
       </body>
-      <GoogleAnalytics gaId={"GTM-TXBK8PXL"} />
+      <GoogleAnalytics gaId={"GTM-MNSWPXXT"} />
     </html>
   );
 }
