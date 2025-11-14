eabddb26a7034b5c95687fad38bc0207f2b61571
Van Quang <quang.tran.van@airstage-vn.asia>
2025-11-14 13:35:31 +0700

    update order of button privacy, term of use

--- src/components/layouts/GlobalLayout.tsx
+++ src/components/layouts/GlobalLayout.tsx
@@ -15,7 +15,7 @@ import { useTheme } from "@/hooks/use-theme";
 export function GlobalLayout({ children }: { children: React.ReactNode }) {
   const t = useTranslations("LandingPage");
   const params = useParams();
-  const pathname = usePathname()
+  const pathname = usePathname();
   const { locale } = params;
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? "https://avatar48.ai";
 
@@ -42,11 +42,7 @@ export function GlobalLayout({ children }: { children: React.ReactNode }) {
           </Link>
           <Link href={`${baseUrl}/platform/${locale}/sentient?mode=${theme}`}>
             <Button className="bg-white text-black hover:opacity-80 cursor-pointer px-3 sm:px-2">
-              <CircleArrowRight
-                size={24}
-                strokeWidth={2}
-                className="hidden sm:block"
-              />
+              <CircleArrowRight size={24} strokeWidth={2} className="hidden sm:block" />
               {t("create_agent")}
             </Button>
           </Link>
@@ -59,7 +55,8 @@ export function GlobalLayout({ children }: { children: React.ReactNode }) {
         className={cn(
           "fixed bottom-7.75 md:bottom-15 lg:bottom-6.75 left-5 md:left-10 lg:left-11.75 hidden gap-2 md:gap-6 2xl:gap-10.5 whitespace-nowrap text-white z-10",
           {
-            "min-[500px]:flex md:flex-col-reverse 2xl:flex-row 2xl:items-center justify-between": pathname === `/${locale}`,
+            "min-[500px]:flex md:flex-col-reverse 2xl:flex-row 2xl:items-center justify-between":
+              pathname === `/${locale}`,
             "min-[500px]:flex flex-row items-center": pathname !== `/${locale}`,
           }
         )}
@@ -67,20 +64,35 @@ export function GlobalLayout({ children }: { children: React.ReactNode }) {
         <LanguageDrawer className="md:w-26.25 h-7 md:h-12.5 rounded-[0.625rem] px-2 md:px-6" />
         <Link
           href={"/term-of-use"}
-          className="uppercase h-7 md:h-12.5 flex-center font-medium text-sm md:text-xl rounded-[0.625rem] bg-white/10 backdrop-blur-md px-2 md:px-6"
+          className={cn(
+            "uppercase h-7 md:h-12.5 flex-center font-medium text-sm md:text-xl rounded-[0.625rem] bg-white/10 backdrop-blur-md px-2 md:px-6",
+            {
+              "order-2 md:order-3 2xl:order-2": pathname === `/${locale}`,
+            }
+          )}
         >
           {t("term_of_use")}
         </Link>
         <Link
           href={"/privacy-policy"}
-          className="uppercase h-7 md:h-12.5 flex-center font-medium text-sm md:text-xl rounded-[0.625rem] bg-white/10 backdrop-blur-md v px-2 md:px-6"
+          className={cn(
+            "uppercase h-7 md:h-12.5 flex-center font-medium text-sm md:text-xl rounded-[0.625rem] bg-white/10 backdrop-blur-md v px-2 md:px-6 order-2",
+            {
+              "order-3 md:order-2 2xl:order-3": pathname === `/${locale}`,
+            }
+          )}
         >
           {t("privacy_policy")}
         </Link>
       </div>
-      <div className={cn("w-max fixed bottom-2.5 min-[500px]:bottom-19 md:bottom-15 lg:bottom-7.75 left-1/2 -translate-x-1/2 z-10", {
-        hidden: pathname !== `/${locale}`
-      })}>
+      <div
+        className={cn(
+          "w-max fixed bottom-2.5 min-[500px]:bottom-19 md:bottom-15 lg:bottom-7.75 left-1/2 -translate-x-1/2 z-10",
+          {
+            hidden: pathname !== `/${locale}`,
+          }
+        )}
+      >
         <BorderGradientWrapper
           gradientType="linear"
           className="p-2 min-[500px]:p-2.5"
@@ -104,9 +116,7 @@ export function GlobalLayout({ children }: { children: React.ReactNode }) {
                 alt={"defaultImage"}
                 className="rounded-xl w-14.75 lg:w-31.25 h-14.75 lg:h-20.5 object-cover"
               />
-              <p className="font-medium text-white text-base md:text-xl">
-                {t("chat_now")}
-              </p>
+              <p className="font-medium text-white text-base md:text-xl">{t("chat_now")}</p>
             </Link>
           </BorderGradientWrapper>
         </BorderGradientWrapper>
